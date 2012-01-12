eb6638a1898d64a825a0be82f3ea09a68751b529
tioover <tioover@gmail.com>
2012-01-12 18:13:38 +0800

    基本重写

--- lifegame.js
+++ lifegame.js
@@ -1,26 +1,29 @@
-var live = true;
-var dead = false;
+var LIVE= true;
+var DEAD= false;
+var XMAX = 1000;
+var YMAX = 1000;
 
-$(document).ready(function(){
-    var matrix = init(50, 25);
-    matrix = set(matrix);
+$(document).ready(function()
+{
+    var matrix = init();
+    matrix = set_all(matrix);
     show(matrix);
+    step_time = 100;
     setInterval(function(){
-        matrix = next(matrix);
+        matrix = nextstep(matrix);
         show(matrix);
-    }, 500);
+    }, step_time);
 });
 
-function trave(matrix, func)
+function through(yfunc)
 {
-    //abstract
-    var max = xymax(matrix);
+    //Through two-dimensional array.
     var x = y = 0;
-    while (x < max.x)
+    while (x < XMAX)
     {
-        while (y < max.y)
+        while(y < YMAX)
         {
-            func(x, y);
+            yfunc(x, y);
             ++ y;
         }
         ++ x;
@@ -28,61 +31,87 @@ function trave(matrix, func)
     }
 }
 
-function init(x_max, y_max)
+function init()
 {
     var matrix = new Array();
-    var x = y = 0;
-    while (x < x_max)
-    {
-        matrix[x] = new Array();
-        while (y < y_max)
-        {
-            matrix[x][y] = dead;
-            ++ y;
-        }
-        ++ x;
-        y = 0;
-    }
-    return matrix;
-}
-
-function set(matrix)
-{
-    //fly
-    /*matrix[40][21] = live;
-    matrix[41][21] = live;
-    matrix[42][21] = live;
-    matrix[40][22] = live;
-    matrix[41][23] = live;*/
-    //long
-    var base_x = 20;
-    matrix[base_x][7] = live;
-    matrix[base_x + 1][1] = live;
-    matrix[base_x + 1][2] = live;
-    matrix[base_x + 2][2] = live;
-    matrix[base_x + 2][6] = live;
-    matrix[base_x + 2][7] = live;
-    matrix[base_x + 2][8] = live;
-    return matrix;
-}
-
-function next(matrix)
-{
-    var max = xymax(matrix);
-    var next_matrix = init(max.x, max.y);
-    trave(matrix, function(x, y){
-        var adja_num = adjacent_cell_num(matrix, x, y);
-        if (adja_num == 2)
-            next_matrix[x][y] = matrix[x][y]; //stable
-        else if (adja_num == 3)
-        {
-            next_matrix[x][y] = live; //relive
-        }
-        else
-        {
-            next_matrix[x][y] = dead; //dead
-        }
+    through(function(x, y){
+        if (matrix[x] == undefined)
+            matrix[x] = new Array();
+        matrix[x][y] = DEAD;
     });
+    return matrix;
+}
+
+function set_all(matrix)
+{
+    set(matrix, 0, 7);
+    set(matrix, 1, 1);
+    set(matrix, 1, 2);
+    set(matrix, 2, 2);
+    set(matrix, 2, 6);
+    set(matrix, 2, 7);
+    set(matrix, 2, 8);
+    return matrix;
+}
+
+function set(matrix, x, y)
+{
+    xoffset = 40;
+    yoffset = 20;
+    matrix[x+xoffset][y+yoffset] = LIVE;
+}
+
+
+function show(matrix)
+{
+    var canvas = document.getElementById("matrix");
+    canvas.width = parseInt(window.screen.availWidth);
+    canvas.height = parseInt(window.screen.availHeight);
+    var context = clear(canvas)
+    var radius = 10;
+    through(function(x, y){
+        var context = cell_painter(canvas, radius, x, y, matrix[x][y]);
+    });
+}
+
+//clear canvas.
+function clear(canvas){
+    var context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    return context;
+}
+
+function cell_painter(canvas, radius, x, y, cell)
+{
+    if (cell)
+    {
+        var dia = radius * 2;
+        var context = canvas.getContext("2d");
+        var centerX = dia * (x);
+        var centerY = dia * (y);
+     
+        context.beginPath();
+        context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+        context.fillStyle = "#9bff9b";
+        context.fill();
+    }
+    return context;
+}
+
+function nextstep(matrix)
+{
+    var next_matrix = init();
+    var STABLE_NUM = 2;
+    var RELIVE_NUM = 3;
+    through(function(x, y){
+        var adja_num = adjacent_cell_num(matrix, x, y);
+        if (adja_num == STABLE_NUM)
+            next_matrix[x][y] = matrix[x][y]; //stable
+        else if (adja_num == RELIVE_NUM)
+            next_matrix[x][y] = LIVE; //relive
+        else
+            next_matrix[x][y] = DEAD; //dead
+    })
     return next_matrix;
 }
 
@@ -94,67 +123,19 @@ function adjacent_cell_num(matrix, x, y)
     var line = function (li){
         if (li != undefined)
         {
-            if (li[y - 1] == live)
+            if (li[y - 1] == LIVE)
                 ++ num;
-            if (li[y] == live)
+            if (li[y] == LIVE)
                 ++ num;
-            if (li[y + 1] == live)
+            if (li[y + 1] == LIVE)
                 ++ num;
         }
     }
     line(matrix[x + 1]);
     line(matrix[x - 1]);
-    if (matrix[x][y - 1] == live)
+    if (matrix[x][y - 1] == LIVE)
         ++ num;
-    if (matrix[x][y + 1] == live)
+    if (matrix[x][y + 1] == LIVE)
         ++ num;
     return num;
 }
-
-function xymax(matrix)
-{
-    //max pos
-    var x_max = matrix.length;
-    var y_max = matrix[0].length;
-    var max = {"x": x_max, "y": y_max};
-    return max;
-}
-
-function show(matrix)
-{
-    //show all cell.
-    var id = "main";
-    var radius = 10;
-    var canvas = document.getElementById(id);
-    clear(canvas);
-    trave(matrix, function(x, y){
-        var context = cell_write(canvas, radius, x, y, matrix[x][y]);
-    });
-    return canvas;
-}
-
-function clear(id){
-    //clear canvas.
-    var id = "main";
-    var canvas = document.getElementById(id);
-    var context = canvas.getContext("2d");
-    context.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-function cell_write(canvas, radius, x, y, cell)
-{
-    var dia = radius * 2;
-    var context = canvas.getContext("2d");
-    var centerX = dia * (x + 1);
-    var centerY = dia * (y + 1);
- 
-    context.beginPath();
-    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    if (cell)
-        context.fillStyle = "#999";
-    else
-        context.fillStyle = "#CCC";
-    context.fill();
-    return context;
-}
-
