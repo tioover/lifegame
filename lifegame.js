95d0ac2a2a4069772af3569d93b585837e23807e
tioover <tioover@gmail.com>
2012-11-16 20:30:00 +0800

    woking

--- lifegame.js
+++ lifegame.js
@@ -1,141 +1,99 @@
-var LIVE= true;
-var DEAD= false;
-var XMAX = 80;
-var YMAX = 80;
+var LIVE = true;
+var DEAD = false;
+var X = 0;
+var Y = 1;
 
-$(document).ready(function()
-{
-  var matrix = init();
-  matrix = set_all(matrix);
-  show(matrix);
-  step_time = 300;
-  setInterval(function(){
-    matrix = nextstep(matrix);
-    show(matrix);
-  }, step_time);
-});
+var lifegame = (function($){
+  var cells = [];
+  var core = {
+    next: function(){},
 
-function through(yfunc)
-{
-  //Through two-dimensional array.
-  var x = y = 0;
-  while (x < XMAX)
-  {
-    while(y < YMAX)
-    {
-      yfunc(x, y);
-      ++ y;
+    around: function(point, step){},
+
+    killer: function(cell) {},
+
+    reviver: function(cell) {},
+
+    is_same_point: function(a, b){},
+
+    make_point: function(x, y){
+      var point = {"x": x, "y": y};
+
+      point.relative_position = function(point){
+        return {x: point.x - this.x, y: point.y - this.y};
+      };
+
+      point.distance = function(point){
+        var position = this.relative_position(point);
+        return {x: Math.abs(position.x), y:Math.abs(position.y)};
+      };
+
+      point.equal = function(point){
+        if(this.x == point.x && this.y == point.y)
+          return true;
+        else
+          return false;
+      }
+
+      point.bigger = function(point){
+        if(this.x > point.x || (this.x == point.x && this.y > point.y))
+          return true;
+        else
+          return false;
+      };
+      return point;
+    },
+
+    get_blank: function(matrix){},
+
+    get_cells: function(matrix){},
+
+    set_cell: function(point){
+      if(cells.length == 0){
+        cells.push(point);
+      }
+      else if(cells.length == 1){
+        if(point.bigger(cells[0])){
+          cells.push(point);}
+        else
+          cells.unshift(point);
+      }
+      else{
+        for(var i in cells){
+          if(point.bigger(cells[cells.length-1]))
+            cells.push(point);
+          if(cells[i].bigger(point)){
+            cells.splice(i, 0, point);
+            break;}
+        }
+      }
     }
-    ++ x;
-    y = 0;
-  }
-}
+  };
 
-function init()
-{
-  var matrix = new Array();
-  through(function(x, y){
-    if (matrix[x] == undefined)
-      matrix[x] = new Array();
-    matrix[x][y] = DEAD;
-  });
-  return matrix;
-}
+  var test = {
+    around: function(){},
+    next: function(){},
+    killer: function(){},
+    reviver: function(){},
+    is_same_point: function(){},
 
-function set_all(matrix)
-{
-  set(matrix, 0, 7);
-  set(matrix, 1, 1);
-  set(matrix, 1, 2);
-  set(matrix, 2, 2);
-  set(matrix, 2, 6);
-  set(matrix, 2, 7);
-  set(matrix, 2, 8);
-  return matrix;
-}
-
-function set(matrix, x, y)
-{
-  xoffset = 40;
-  yoffset = 20;
-  matrix[x+xoffset][y+yoffset] = LIVE;
-}
-
-
-function show(matrix)
-{
-  var canvas = document.getElementById("matrix");
-  canvas.width = parseInt(window.screen.availWidth);
-  canvas.height = parseInt(window.screen.availHeight);
-  var context = clear(canvas)
-  var radius = 10;
-  through(function(x, y){
-    var context = cell_painter(canvas, radius, x, y, matrix[x][y]);
-  });
-}
-
-//clear canvas.
-function clear(canvas){
-  var context = canvas.getContext("2d");
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  return context;
-}
-
-function cell_painter(canvas, radius, x, y, cell)
-{
-  if (cell)
-  {
-    var dia = radius * 2;
-    var context = canvas.getContext("2d");
-    var centerX = dia * (x);
-    var centerY = dia * (y);
- 
-    context.beginPath();
-    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = "#9bff9b";
-    context.fill();
-  }
-  return context;
-}
-
-function nextstep(matrix)
-{
-  var next_matrix = init();
-  var STABLE_NUM = 2;
-  var RELIVE_NUM = 3;
-  through(function(x, y){
-    var adja_num = adjacent_cell_num(matrix, x, y);
-    if (adja_num == STABLE_NUM)
-      next_matrix[x][y] = matrix[x][y]; //stable
-    else if (adja_num == RELIVE_NUM)
-      next_matrix[x][y] = LIVE; //relive
-    else
-      next_matrix[x][y] = DEAD; //dead
-  })
-  return next_matrix;
-}
-
-function adjacent_cell_num(matrix, x, y)
-{
-  //Number of adjacent grid
-  var cell = matrix[x][y];
-  var num = 0;
-  var line = function (li){
-    if (li != undefined)
-    {
-      if (li[y - 1] == LIVE)
-        ++ num;
-      if (li[y] == LIVE)
-        ++ num;
-      if (li[y + 1] == LIVE)
-        ++ num;
+    set_cells: function(){
+      var add = function(x, y){
+        var p = core.make_point(x,y);
+        core.set_cell(p);
+      };
+      add(20,1);
+      add(10,1);
+      add(0,11);
+      add(10,29);
+      add(10,100);
+      add(20,100);
+      console.log(cells);
     }
-  }
-  line(matrix[x + 1]);
-  line(matrix[x - 1]);
-  if (matrix[x][y - 1] == LIVE)
-    ++ num;
-  if (matrix[x][y + 1] == LIVE)
-    ++ num;
-  return num;
-}
+  };
+
+  return {
+    core: core,
+    cells: cells,
+    test: test};
+})(jQuery);
---
+++ lifegame.old.js
@@ -0,0 +1,141 @@
+var LIVE= true;
+var DEAD= false;
+var XMAX = 80;
+var YMAX = 80;
+
+$(document).ready(function()
+{
+  var matrix = init();
+  matrix = set_all(matrix);
+  show(matrix);
+  step_time = 3000;
+  setInterval(function(){
+    matrix = nextstep(matrix);
+    show(matrix);
+  }, step_time);
+});
+
+function through(yfunc)
+{
+  //Through two-dimensional array.
+  var x = y = 0;
+  while (x < XMAX)
+  {
+    while(y < YMAX)
+    {
+      yfunc(x, y);
+      ++ y;
+    }
+    ++ x;
+    y = 0;
+  }
+}
+
+function init()
+{
+  var matrix = new Array();
+  through(function(x, y){
+    if (matrix[x] == undefined)
+      matrix[x] = new Array();
+    matrix[x][y] = DEAD;
+  });
+  return matrix;
+}
+
+function set_all(matrix)
+{
+  set(matrix, 0, 7);
+  set(matrix, 1, 1);
+  set(matrix, 1, 2);
+  set(matrix, 2, 2);
+  set(matrix, 2, 6);
+  set(matrix, 2, 7);
+  set(matrix, 2, 8);
+  return matrix;
+}
+
+function set(matrix, x, y)
+{
+  xoffset = 40;
+  yoffset = 20;
+  matrix[x+xoffset][y+yoffset] = LIVE;
+}
+
+
+function show(matrix)
+{
+  var canvas = document.getElementById("matrix");
+  canvas.width = parseInt(window.screen.availWidth);
+  canvas.height = parseInt(window.screen.availHeight);
+  var context = clear(canvas)
+  var radius = 10;
+  through(function(x, y){
+    var context = cell_painter(canvas, radius, x, y, matrix[x][y]);
+  });
+}
+
+//clear canvas.
+function clear(canvas){
+  var context = canvas.getContext("2d");
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  return context;
+}
+
+function cell_painter(canvas, radius, x, y, cell)
+{
+  if (cell)
+  {
+    var dia = radius * 2;
+    var context = canvas.getContext("2d");
+    var centerX = dia * (x);
+    var centerY = dia * (y);
+ 
+    context.beginPath();
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = "#9bff9b";
+    context.fill();
+  }
+  return context;
+}
+
+function nextstep(matrix)
+{
+  var next_matrix = init();
+  var STABLE_NUM = 2;
+  var RELIVE_NUM = 3;
+  through(function(x, y){
+    var adja_num = adjacent_cell_num(matrix, x, y);
+    if (adja_num == STABLE_NUM)
+      next_matrix[x][y] = matrix[x][y]; //stable
+    else if (adja_num == RELIVE_NUM)
+      next_matrix[x][y] = LIVE; //relive
+    else
+      next_matrix[x][y] = DEAD; //dead
+  })
+  return next_matrix;
+}
+
+function adjacent_cell_num(matrix, x, y)
+{
+  //Number of adjacent grid
+  var cell = matrix[x][y];
+  var num = 0;
+  var line = function (li){
+    if (li != undefined)
+    {
+      if (li[y - 1] == LIVE)
+        ++ num;
+      if (li[y] == LIVE)
+        ++ num;
+      if (li[y + 1] == LIVE)
+        ++ num;
+    }
+  }
+  line(matrix[x + 1]);
+  line(matrix[x - 1]);
+  if (matrix[x][y - 1] == LIVE)
+    ++ num;
+  if (matrix[x][y + 1] == LIVE)
+    ++ num;
+  return num;
+}
